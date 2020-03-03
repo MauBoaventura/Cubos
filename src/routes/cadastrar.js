5761586142254e48ff13ee96e805b9a57bd5a00f
Mauricio Boaventura <mba097@gmail.com>
2020-03-03 04:13:27 -0300

    Transformando uma data em um obj moment

--- src/routes/cadastrar.js
+++ src/routes/cadastrar.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
-router.post('/', (req, res) => {
-    
-    res.status(201).send(req.body)
+//Uso de Datas
+const moment = require("moment")
+
+router.post('/especifico', (req, res) => {
+    const DataInicio = req.body.longI;
+    const DataFim = req.body.longF;
+
+    console.log("Data Inicio: " + moment(DataInicio).format("DD-MM-YYYY hh:mm:ss"))
+    console.log("Data Fim   : " + moment(DataFim).format("DD-MM-YYYY hh:mm:ss"))
+    console.log("Data Agora : " + moment().format("DD-MM-YYYY hh:mm:ss"))
+    console.log("")
+
+
+    res.status(201).send(DataInicio + " " + DataFim)
+})
+
+router.post('/diario', (req, res) => {
+    const operacao = req.body.operacao;
+
+    res.status(201).send(operacao)
+})
+router.post('/semanal', (req, res) => {
+    const operacao = req.body.operacao;
+
+    res.status(201).send(operacao)
 })
 
 
