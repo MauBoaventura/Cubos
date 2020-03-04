302c1173b307384c37bfb02687d9b8759d0fb271
Mauricio Boaventura <mba097@gmail.com>
2020-03-04 01:40:13 -0300

    Deletar pelo id

--- src/routes/deletar.js
+++ src/routes/deletar.js
@@ -1,26 +1,50 @@
 const express = require("express");
 const router = express.Router();
 
-router.delete('/:id', (req, res) =>{
-    var id=req.body.id
+//Manipular arquivos
+const fs = require('fs');
+
+router.delete('/:id', (req, res) => {
+    var id = req.params.id
     //Ler json salvo em data
     let rawdata = fs.readFileSync("src\\data\\data.json");
     let dados = JSON.parse(rawdata);
 
-    //Insere na variavel
-    dados.horarios.splice(id)
+    let msg = removerPorId(dados.horarios, id)
 
-    //Inserir novo cadastro no arquivo json
-    fs.writeFile("src\\data\\data.json", JSON.stringify(dados), function (erro) {
-        if (erro) {
-            throw erro;
-        }
-        console.log("Atendimento salvo");
+    console.log(msg)
+    //Se ocorre um erro uma msg é exibida
+    if (msg != undefined) {
+        res.status(400).send(msg)
+    } else {
+
+        // Inserir novo cadastro no arquivo json
+        fs.writeFile("src\\data\\data.json", JSON.stringify(dados), function (erro) {
+            if (erro) {
+                throw erro;
+            }
+        });
+
+        res.status(200).send(dados)
+    }
+
+})
+
+//Funcao que percorre o array e apaga o id
+function removerPorId(array, id) {
+
+    var result = array.filter(function (el) {
+        return el.id == id;
     });
 
-    res.status(201).send(salvar)
+    if (result == undefined || result == null || result == "") {
+        return "ID não encontrado"
 
-} )
-
+    }
+    for (var elemento of result) {
+        var index = array.indexOf(elemento);
+        array.splice(index, 1);
+    }
+}
 
 module.exports = router;
